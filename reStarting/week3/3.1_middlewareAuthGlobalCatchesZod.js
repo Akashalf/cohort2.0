617179c8e847a367b5b3ff05d37f055cfea27355
Akash Shukla <akashalf719@gmail.com>
2025-11-17 14:49:29 +0530

    updated 3.1 now working fine

--- reStarting/week3/3.1_middlewareAuthGlobalCatchesZod.js
+++ reStarting/week3/3.1_middlewareAuthGlobalCatchesZod.js
@@ -1,28 +1,23 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-
 app.get("/", (req, res) => {
-    console.log(req.body);        //not work without body-parser
-    console.log(req.headers);
-    res.send(`
-        <html>
-            <title>My page</title>
-            <body>
-                <h1>Long time no see!!</h1>
-                <h2>Hey how are you!<h2>
-            </body>
-        </html>
-    `);
+    let kidneyId = Number(req.query.kidneyId);
+    let userName = req.headers.username;
+    let pass = req.headers.pass;
+
+    if (userName !== "Akash" || pass !== "pass") {
+        return res.status(403).json({ msg: "Wrong Credentials" });
+    }
+
+    if (kidneyId !== 1 && kidneyId !== 2) {
+        return res.status(411).json({ msg: "Wrong Input" });
+    }
+
+    return res.status(200).json({ msg: "Success" });
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`App is listening on port ${port}.`);
 });
-
-
-
-
